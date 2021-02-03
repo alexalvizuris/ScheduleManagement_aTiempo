5cec151ab6d0fc6b501282e0172cd1467b0d8136
alexalvizuris <39359044+alexalvizuris@users.noreply.github.com>
2021-02-02 23:46:53 -0800

    Initial commit

---
+++ src/module-info.java
@@ -0,0 +1,14 @@
+module ScheduleManagement.aTiempo {
+
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires java.sql;
+    requires mysql.connector.java;
+
+    opens controller;
+    //opens database;
+    opens main;
+   // opens utility;
+    opens view;
+
+}
